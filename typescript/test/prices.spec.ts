831d03cd476222c828a36a70ab307ff7a4102a82
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-03-09 16:30:22 -0500

    Reformat code with prettier

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -1,26 +1,22 @@
-import {assert, expect} from 'chai';
-import request from 'supertest-as-promised';
-import {createApp} from "../src/prices"
+import { assert, expect } from "chai";
+import request from "supertest-as-promised";
+import { createApp } from "../src/prices";
 
-describe('prices', () => {
+describe("prices", () => {
+  let app, connection;
 
-    let app, connection
+  beforeEach(async () => {
+    ({ app, connection } = await createApp());
+  });
 
-    beforeEach(async () => {
-        ({app, connection} = await createApp());
-    });
+  afterEach(function () {
+    connection.close();
+  });
 
-    afterEach(function () {
-        connection.close()
-    });
+  it("does something", async () => {
+    const response = await request(app).get("/prices?type=1jour");
 
-    it('does something', async () => {
-
-        const response = await request(app)
-            .get('/prices?type=1jour')
-
-        var exptectedResult = {cost: 35} // change this to make the test pass
-        expect(response.body).deep.equal(exptectedResult)
-    });
-
+    var exptectedResult = { cost: 35 }; // change this to make the test pass
+    expect(response.body).deep.equal(exptectedResult);
+  });
 });

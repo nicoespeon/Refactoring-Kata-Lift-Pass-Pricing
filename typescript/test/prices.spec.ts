4348947ccadc44ae16de8380c0ae2b073363c9f4
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-03-09 17:07:33 -0500

    Add more tests

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -26,10 +26,34 @@ describe("prices", () => {
     connection.close();
   });
 
-  it("does something", async () => {
+  it("returns 35 for 1jour", async () => {
     const response = await request(app).get("/prices?type=1jour");
 
     const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 0 for night", async () => {
+    const response = await request(app).get("/prices?type=night");
+
+    const expectedResult = { cost: 0 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 19 for night and age is 14", async () => {
+    const response = await request(app).get("/prices?type=night&age=14");
+
+    const expectedResult = { cost: 19 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 8 for night and age is 65", async () => {
+    const response = await request(app).get("/prices?type=night&age=65");
+
+    const expectedResult = { cost: 8 };
 
     expect(response.body).deep.equal(expectedResult);
   });

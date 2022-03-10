c174a70aa00ff260efc07d12dcc633890915eab4
Guillaume Lagorce <glagorce@turbulent.ca>
2022-03-09 21:19:24 -0500

    cover all cases with high level tests

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -57,4 +57,93 @@ describe("prices", () => {
 
     expect(response.body).deep.equal(expectedResult);
   });
+
+  it("returns 0 for night and age is 5", async () => {
+    const response = await request(app).get("/prices?type=night&age=5");
+
+    const expectedResult = { cost: 0 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 8 for 1jour and age is 14", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=14");
+
+    const expectedResult = { cost: 25 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 27 for 1jour and age is 65", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=65");
+
+    const expectedResult = { cost: 27 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+
+  it("returns 35 for 1jour and age is 64", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=64");
+
+    const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 35 for 1jour and age is 15", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15");
+
+    const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 23 for 1jour and age is 15 on 2022-02-22", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2022-02-22");
+
+    const expectedResult = { cost: 23 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 23 for 1jour and age is 15 on 2022-02-29", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2022-02-29");
+
+    const expectedResult = { cost: 23 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 35 for 1jour and age is 15 on 2022-02-28", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2022-02-28");
+
+    const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 35 for 1jour and age is 15 on 2019-02-18", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2019-02-18T05:00:00.000Z");
+
+    const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 35 for 1jour and age is 15 on 2019-02-25", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2019-02-25T05:00:00.000Z");
+
+    const expectedResult = { cost: 35 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
+
+  it("returns 23 for 1jour and age is 15 on 2019-02-04", async () => {
+    const response = await request(app).get("/prices?type=1jour&age=15&date=2019-02-04T05:00:00.000Z");
+
+    const expectedResult = { cost: 23 };
+
+    expect(response.body).deep.equal(expectedResult);
+  });
 });

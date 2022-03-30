65692dd5d2e66ad873ea131e64de20611bf204b3
Nicolas Carlo <nicolascarlo.espeon@gmail.com>
2022-03-30 17:05:21 -0400

    Run Prettier over specs

--- typescript/test/prices.spec.ts
+++ typescript/test/prices.spec.ts
@@ -4,11 +4,11 @@ import { GenericContainer, StartedTestContainer } from "testcontainers";
 import { createApp } from "../src/prices";
 
 type TestCase = {
-  type: '1jour' | 'night',
-  age?: number | undefined,
-  date?: Date | undefined,
-  expectedCost: number,
-}
+  type: "1jour" | "night";
+  age?: number | undefined;
+  date?: Date | undefined;
+  expectedCost: number;
+};
 
 describe("prices", () => {
   let app, connection, container: StartedTestContainer;
@@ -26,54 +26,60 @@ describe("prices", () => {
   });
 
   beforeEach(async () => {
-    ({ app, connection } = await createApp({ port: container.getMappedPort(3306) }));
+    ({ app, connection } = await createApp({
+      port: container.getMappedPort(3306),
+    }));
   });
 
   afterEach(() => {
     connection.close();
   });
 
-
-  describe('when type is night', () => {
+  describe("when type is night", () => {
     const nightTestCases: TestCase[] = [
-      { type: 'night', expectedCost: 0 },
-      { type: 'night', expectedCost: 19, age: 14 },
-      { type: 'night', expectedCost: 8, age: 65 },
-      { type: 'night', expectedCost: 0, age: 5 },
+      { type: "night", expectedCost: 0 },
+      { type: "night", expectedCost: 19, age: 14 },
+      { type: "night", expectedCost: 8, age: 65 },
+      { type: "night", expectedCost: 0, age: 5 },
     ];
     runTestCases(nightTestCases);
   });
 
-  describe('when type is 1jour', () => {
+  describe("when type is 1jour", () => {
     const dayTestCases: TestCase[] = [
-      { type: '1jour', expectedCost: 35 },
-      { type: '1jour', expectedCost: 25, age: 14 },
-      { type: '1jour', expectedCost: 27, age: 65 },
-      { type: '1jour', expectedCost: 35, age: 64 },
-      { type: '1jour', expectedCost: 35, age: 15 },
+      { type: "1jour", expectedCost: 35 },
+      { type: "1jour", expectedCost: 25, age: 14 },
+      { type: "1jour", expectedCost: 27, age: 65 },
+      { type: "1jour", expectedCost: 35, age: 64 },
+      { type: "1jour", expectedCost: 35, age: 15 },
     ];
     runTestCases(dayTestCases);
 
-    const monday1 = new Date('2022-02-21T12:00:00');
-    const monday2 = new Date('2022-02-21T12:00:00');
-    const holidayMonday1 = new Date('2019-02-18T12:00:00');
-    const holidayMonday2 = new Date('2019-02-25T12:00:00');
-    const holidayMonday3 = new Date('2019-03-04T12:00:00');
+    const monday1 = new Date("2022-02-21T12:00:00");
+    const monday2 = new Date("2022-02-21T12:00:00");
+    const holidayMonday1 = new Date("2019-02-18T12:00:00");
+    const holidayMonday2 = new Date("2019-02-25T12:00:00");
+    const holidayMonday3 = new Date("2019-03-04T12:00:00");
 
-    describe('when date is a monday', () => {
+    describe("when date is a monday", () => {
       const mondayTestCases: TestCase[] = [
-        { type: '1jour', expectedCost: 23, age: 15, date: monday1 },
-        { type: '1jour', expectedCost: 23, age: 15, date: monday2 },
-        { type: '1jour', expectedCost: 35, age: 15, date: holidayMonday1 },
-        { type: '1jour', expectedCost: 35, age: 15, date: holidayMonday2 },
-        { type: '1jour', expectedCost: 35, age: 15, date: holidayMonday3 },
+        { type: "1jour", expectedCost: 23, age: 15, date: monday1 },
+        { type: "1jour", expectedCost: 23, age: 15, date: monday2 },
+        { type: "1jour", expectedCost: 35, age: 15, date: holidayMonday1 },
+        { type: "1jour", expectedCost: 35, age: 15, date: holidayMonday2 },
+        { type: "1jour", expectedCost: 35, age: 15, date: holidayMonday3 },
       ];
       runTestCases(mondayTestCases);
-    })
+    });
 
-    const sunday = new Date('2022-02-20T12:00:00');
-    describe('when date is a sunday', () => {
-      const sundayTestCase: TestCase = { type: '1jour', expectedCost: 35, age: 15, date: sunday };
+    const sunday = new Date("2022-02-20T12:00:00");
+    describe("when date is a sunday", () => {
+      const sundayTestCase: TestCase = {
+        type: "1jour",
+        expectedCost: 35,
+        age: 15,
+        date: sunday,
+      };
       runTestCase(sundayTestCase);
     });
   });
@@ -86,8 +92,12 @@ describe("prices", () => {
 
   function runTestCase(testCase: TestCase) {
     let description = `returns ${testCase.expectedCost}`;
-    description = testCase.age ? `${description} - age is ${testCase.age}` : description;
-    description = testCase.date ? `${description} and date is ${testCase.date}` : description;
+    description = testCase.age
+      ? `${description} - age is ${testCase.age}`
+      : description;
+    description = testCase.date
+      ? `${description} and date is ${testCase.date}`
+      : description;
 
     it(description, async () => {
       let pricesQuery = `/prices?type=${testCase.type}`;
